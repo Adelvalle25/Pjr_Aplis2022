85e4c1c74df8b6004baaaa3f140ff6142a6c0e9e
Adelvalle25 <andres.delvalle@galileo.edu>
2022-06-04 21:13:28 -0600

    v1/ add config

--- Proyecto_v1/FLASH/Sources/main_c.d
+++ Proyecto_v1/FLASH/Sources/main_c.d
@@ -5,5 +5,7 @@ Sources/main_c.obj: \
 	C:/Freescale/CW\ MCU\ v11.1/MCU/lib/hc08c/include/stddef.h \
 	C:/Freescale/CW\ MCU\ v11.1/MCU/lib/hc08c/include/stdtypes.h \
 	C:/Users/aadel/Desktop/Pjr_Aplis2022/Proyecto_v1/Project_Headers/derivative.h \
-	C:/Users/aadel/Desktop/Pjr_Aplis2022/Proyecto_v1/Project_Headers/mc9s08sh8.h 
+	C:/Users/aadel/Desktop/Pjr_Aplis2022/Proyecto_v1/Project_Headers/mc9s08sh8.h \
+	C:/Freescale/CW\ MCU\ v11.1/MCU/lib/hc08c/include/stdlib.h \
+	C:/Freescale/CW\ MCU\ v11.1/MCU/lib/hc08c/include/libdefs.h 
 
--- Proyecto_v1/Sources/main.c
+++ Proyecto_v1/Sources/main.c
@@ -1,41 +1,62 @@
 #include <hidef.h> /* for EnableInterrupts macro */
 #include "derivative.h" /* include peripheral declarations */
+#include <stdlib.h> /* Para realizar funcion div */
 
-void init_motor1(void){
-	//PTB5 DIGITAL
-	//TPM2CH1 PWM
-}
-int chPW1(int vel){
-	return 1;
-}
-
-
-void init_motor2(void){
-	//PTB3 DIGITAL
-	//TPM1CH0 PWM
-}
-int chPW2(int vel){
-	return 1;
-}
-
-void init_servo(void){
-	//TPM1CH1 PWM
-}
-void angle(int ang){
-	
-}
-
-void init_ultrasonico(void){
-	//ADP10
-}
-
-void init_optico(void){
-	//ADP11
-}
 
 int map(int value, int RIE,int RFE, int RIS, int RFS){
 	return (((RFS-RIS)/(RFE-RIE))*(value)+RIS);
 }
+
+void init_motor1(void){
+	PTBDD_PTBDD5=1;
+	TPM1SC=0xB;
+	TPM2MOD=1000;
+	TPM2C1SC=0x28;
+	//PTB5 DIGITAL
+	//TPM2CH1 PWM
+}
+void chPW1(int vel){
+	TPM2C1V=vel;
+}
+
+
+void init_motor2(void){
+	PTBDD_PTBDD3=1;
+	TPM1SC=0xB;
+	TPM1MOD=1000;
+	TPM1C1SC=0x28;
+	//PTB3 DIGITAL
+	//TPM1CH0 PWM
+}
+void chPW2(int vel){
+	TPM1C0V=vel;
+}
+
+void init_servo(void){
+	TPM1SC=0xA;
+	TPM1MOD=40000;
+	TPM1C1SC=0x28;
+	//TPM1CH1 PWM
+}
+void angle(int ang){
+	TPM1C1V=ang;
+}
+
+void init_ultrasonico(void){
+	ADCCFG = 0xE0;
+	APCTL2 = 0x4;
+	ADCSC1 = 0x6A;
+	//ADP10
+}
+
+void init_optico(void){
+	ADCCFG = 0xE0;
+	APCTL2 = 0x8;
+	ADCSC1 = 0x6B;
+	//ADP11
+}
+
+
 
 
 void main(void) {
